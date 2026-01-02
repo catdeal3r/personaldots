8c7ad5018dfb9ad1c1cec564be2bfdb937e43205
dealerofallthecats <xhackerqwertyx@protonmail.com>
2026-01-03 12:40:07 +1300

    fix typo

--- .config/labwc/wallset.sh
+++ .config/labwc/wallset.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-swaybg -m fill -i "/run/media/catdealer/Data/Pictures/wallpapers/vintage/kaylin-pacheco-YL8fzqM_EN0-unsplash.jpg" & disown
+swaybg -m fill -i /run/media/catdealer/Data/Pictures/wallpapers/art/clouds-landscape.jpg & disown
